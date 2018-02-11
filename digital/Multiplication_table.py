8b5e3402b7ee6c0233d1ac3066304c51c9f3e419
magicmshing <31586983+magicmshing@users.noreply.github.com>
2018-02-11 15:54:35 +0800

    Multiplication_table
    Multiplication_table

---
+++ digital/Multiplication_table.py
@@ -0,0 +1,14 @@
+for i in range (1, 10):
+    for j in range (1, i + 1):
+        print ('{0}*{1}={2}'.format (i, j, i * j), end = ' ')
+    print ()
+
+i, j = 1, 1
+n = int(input())
+for i in range(n + 1) :
+    for j in range(i) :
+        j += 1
+        print ( '{0}*{1}={2}'.format (i, j, i * j), end = ' ')
+    i += 1
+    if i != j :
+        print ('')
